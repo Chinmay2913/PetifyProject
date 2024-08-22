702caf14e1e516d949a08bfdfdf25d34546e2841
Shivtej Nikam <111061819+Shivtej-Nikam@users.noreply.github.com>
2024-08-22 14:12:23 +0530

    controller layer

---
+++ controller/CartItemController.java
@@ -0,0 +1,58 @@
+package com.zosh.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zosh.exception.CartItemException;
+import com.zosh.exception.UserException;
+import com.zosh.modal.CartItem;
+import com.zosh.modal.User;
+import com.zosh.response.ApiResponse;
+import com.zosh.service.CartItemService;
+import com.zosh.service.UserService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@RestController
+@RequestMapping("/api/cart_items")
+@Tag(name="Cart Item Management", description = "create cart item delete cart item")
+public class CartItemController {
+
+	private CartItemService cartItemService;
+	private UserService userService;
+	
+	public CartItemController(CartItemService cartItemService,UserService userService) {
+		this.cartItemService=cartItemService;
+		this.userService=userService;
+	}
+	
+	@DeleteMapping("/{cartItemId}")
+	public ResponseEntity<ApiResponse>deleteCartItemHandler(@PathVariable Long cartItemId, @RequestHeader("Authorization")String jwt) throws CartItemException, UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		cartItemService.removeCartItem(user.getId(), cartItemId);
+		
+		ApiResponse res=new ApiResponse("Item Remove From Cart",true);
+		
+		return new ResponseEntity<ApiResponse>(res,HttpStatus.ACCEPTED);
+	}
+	
+	@PutMapping("/{cartItemId}")
+	public ResponseEntity<CartItem>updateCartItemHandler(@PathVariable Long cartItemId, @RequestBody CartItem cartItem, @RequestHeader("Authorization")String jwt) throws CartItemException, UserException{
+		
+		User user=userService.findUserProfileByJwt(jwt);
+		
+		CartItem updatedCartItem =cartItemService.updateCartItem(user.getId(), cartItemId, cartItem);
+		
+		//ApiResponse res=new ApiResponse("Item Updated",true);
+		
+		return new ResponseEntity<>(updatedCartItem,HttpStatus.ACCEPTED);
+	}
+}
---
+++ controller/RatingController.java
@@ -0,0 +1,50 @@
+package com.zosh.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zosh.exception.ProductException;
+import com.zosh.exception.UserException;
+import com.zosh.modal.Rating;
+import com.zosh.modal.Review;
+import com.zosh.modal.User;
+import com.zosh.request.RatingRequest;
+import com.zosh.service.RatingServices;
+import com.zosh.service.UserService;
+
+@RestController
+@RequestMapping("/api/ratings")
+public class RatingController {
+	
+	private UserService userService;
+	private RatingServices ratingServices;
+	
+	public RatingController(UserService userService,RatingServices ratingServices) {
+		this.ratingServices=ratingServices;
+		this.userService=userService;
+		// TODO Auto-generated constructor stub
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<Rating> createRatingHandler(@RequestBody RatingRequest req,@RequestHeader("Authorization") String jwt) throws UserException, ProductException{
+		User user=userService.findUserProfileByJwt(jwt);
+		Rating rating=ratingServices.createRating(req, user);
+		return new ResponseEntity<>(rating,HttpStatus.ACCEPTED);
+	}
+	
+	@GetMapping("/product/{productId}")
+	public ResponseEntity<List<Rating>> getProductsReviewHandler(@PathVariable Long productId){
+	
+		List<Rating> ratings=ratingServices.getProductsRating(productId);
+		return new ResponseEntity<>(ratings,HttpStatus.OK);
+	}
+}
